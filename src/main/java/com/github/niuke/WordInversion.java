2943554adaecd7ba03d7d2ba5a0002d7bee9932a
kangpanwork <kangpan_work@163.com>
2021-09-13 20:35:57 +0800

    update

--- src/main/java/com/github/niuke/WordInversion.java
+++ src/main/java/com/github/niuke/WordInversion.java
@@ -17,7 +17,8 @@ public class WordInversion {
             String[] words = str.split("[^A-Za-z]");
             StringBuilder result = new StringBuilder();
             for (int i = words.length - 1; i >= 0; i--) {
-                result.append(words[i]).append(" ");
+                StringBuilder stringBuilders = new StringBuilder(words[i]);
+                result.append(stringBuilders.reverse()).append(" ");
             }
             System.out.println(result.toString().trim());
         }

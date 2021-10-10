cc4fb74068645acb86dc78162261131cda92a509
kangpanwork <kangpan_work@163.com>
2021-10-10 22:09:37 +0800

    update

--- src/main/java/com/github/mvc/repository/UserRepository.java
+++ src/main/java/com/github/mvc/repository/UserRepository.java
@@ -3,14 +3,20 @@ package com.github.mvc.repository;
 import com.github.mvc.model.User;
 import lombok.Data;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Collection;
+import java.util.Map;
 
 /**
+ * IOC
  * 依赖注入 依赖查找
+ * 依赖来源：自定义bean 内建依赖 内建bean
  *
  * @author 康盼Java开发工程师
  */
@@ -27,7 +33,8 @@ public class UserRepository {
     private ObjectFactory<ApplicationContext> applicationContextObjectFactory;
 
     public static void main(String[] args) {
-        BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:spring-bean.xml");
+        String location = "classpath:spring-bean.xml";
+        BeanFactory beanFactory = new ClassPathXmlApplicationContext(location);
         UserRepository userRepository = beanFactory.getBean("userRepository",UserRepository.class);
         System.out.println(userRepository.getBeanFactory());
         // false
@@ -36,5 +43,15 @@ public class UserRepository {
         System.out.println(objectFactory.getObject());
         // true (ApplicationContext  BeanFactory)
         System.out.println(userRepository.getApplicationContextObjectFactory().getObject() == beanFactory);
+
+        DefaultListableBeanFactory defaultListableBeanFactory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(defaultListableBeanFactory);
+        reader.loadBeanDefinitions(location);
+        if (defaultListableBeanFactory instanceof ListableBeanFactory) {
+            ListableBeanFactory listableBeanFactory = defaultListableBeanFactory;
+            Map<String, User> users = listableBeanFactory.getBeansOfType(User.class);
+            System.out.println("查找所有的 user 集合对象：" + users);
+        }
+
     }
 }
